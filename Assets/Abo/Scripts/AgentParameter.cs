92b1d78571fd966f9d2576936eb33ddec3a430df
TOFGAMES <tofgames555@gmail.com>
2020-05-05 14:49:06 +0900

    unirxいれた

---
+++ Assets/Abo/Scripts/AgentParameter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class AgentParameter : MonoBehaviour {
+	//=============================================================
+	
+
+	//=============================================================
+	private void Init(){
+	}
+
+	private void Awake () {
+		Init();
+	}
+
+	private void Start () {
+		
+	}
+	
+	private void Update () {
+		
+	}
+}
